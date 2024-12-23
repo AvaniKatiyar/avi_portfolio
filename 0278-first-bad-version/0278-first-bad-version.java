0f6f61064ea6e2ec903a1a92d780190f979a9d0e
AvaniKatiyar <2k23.cscys2310960@gmail.com>
2024-12-23 22:35:40 +0530

    Time: 24 ms (98.15%), Space: 40.3 MB (50.08%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,19 +3,17 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int first = 1;
-        int last = n;
-
-        while (first < last) {
-            int mid = first + (last - first) / 2;
-
-            if (isBadVersion(mid)) {
-                last = mid; // The first bad version could be mid or before.
-            } else {
-                first = mid + 1; // The first bad version must be after mid.
+        int l=1;
+        int r=n;
+        while(l<r)
+        {
+            int version=l+(r-l)/2;
+            if(isBadVersion(version))
+            {
+                r=version;
             }
+            else {l=version+1;}
         }
-
-        return first; // At the end, first will be the first bad version.
+        return l;
     }
 }
