51539bf334b7493278f2e16f5bcfba6e060dae24
AvaniKatiyar <2k23.cscys2310960@gmail.com>
2025-04-29 21:22:42 +0530

    Time: 0 ms (100%), Space: 43 MB (8.43%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,23 +10,63 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-         ListNode dummy = new ListNode();
-        ListNode cur = dummy;
-
-        while (list1 != null && list2 != null) {
-            if (list1.val > list2.val) {
-                cur.next = list2;
-                list2 = list2.next;
-            } else {
-                cur.next = list1;
-                list1 = list1.next;
+        ListNode head=null;
+        ListNode temp=null;
+        int num;
+        while(list1!=null && list2!=null)
+        {
+            if(list1.val<=list2.val)
+            {
+              num=list1.val; 
+              list1=list1.next; 
             }
-            cur = cur.next;
+            else
+            {num=list2.val;
+            list2=list2.next;}
+            ListNode newnode= new ListNode(num);
+            if(head==null)
+            {
+              head=temp=newnode;
+            }
+            else {
+                temp.next=newnode;
+                temp=temp.next;
+            }
         }
-
-        cur.next = (list1 != null) ? list1 : list2;
-
-        return dummy.next;        
-        
+        if(list1==null &&list2!=null)
+        {
+            while(list2!=null)
+            {
+           num=list2.val;
+           list2=list2.next;
+           ListNode newnode= new ListNode(num);
+            if(head==null)
+            {
+              head=temp=newnode;
+            }
+            else {
+                temp.next=newnode;
+                temp=temp.next;
+            }
+            }
+        }
+        else 
+        {
+           while(list1!=null)
+           {
+           num=list1.val;
+           list1=list1.next;
+           ListNode newnode= new ListNode(num); 
+           if(head==null)
+            {
+              head=temp=newnode;
+            }
+            else {
+                temp.next=newnode;
+                temp=temp.next;
+            } 
+         }
+        }
+        return head;
     }
 }
