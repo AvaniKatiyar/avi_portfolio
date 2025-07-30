9c7f9a196da89f452157b888db49503c3d6febed
AvaniKatiyar <2k23.cscys2310960@gmail.com>
2025-07-30 15:16:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int l=nums.length;
+        if(nums[0]!=0){return 1;}
+        
+        for(int i=0;i<l-1;i++){
+            if(nums[i+1]!=nums[i]+1)
+            {
+               return nums[i]+1;
+            }
+        }
+        if(l-1==nums[l-1])
+        {
+          return l;  
+        }
+        return 0;
+    }
+}
