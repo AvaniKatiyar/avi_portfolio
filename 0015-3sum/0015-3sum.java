bf4aacb5a35b43cb5d3efab672ad80d5ddcb1c1f
AvaniKatiyar <2k23.cscys2310960@gmail.com>
2025-08-07 00:15:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,24 @@
+
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int target = -nums[i];
+            Set<Integer> seen = new HashSet<>();
+            for (int j = i + 1; j < n; j++) {
+                int complement = target - nums[j];
+                if (seen.contains(complement)) {
+                    // Sort triplet before adding to avoid duplicates
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
+                    Collections.sort(triplet);
+                    result.add(triplet);
+                }
+                seen.add(nums[j]);
+            }
+        }
+        return new ArrayList<>(result);
+    }
+}
