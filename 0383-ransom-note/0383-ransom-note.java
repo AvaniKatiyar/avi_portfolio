b9077066b043ecb60d896cebdecc4d02ef8da564
AvaniKatiyar <2k23.cscys2310960@gmail.com>
2025-08-22 15:49:34 +0530

    Time: 11 ms (38.54%), Space: 45.2 MB (47%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,6 +1,18 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        
-        return(magazine.contains(ransomNote));
+        HashMap<Character,Integer> mag_hash=new HashMap<>();
+        for(char c: magazine.toCharArray())
+        {
+            mag_hash.put(c, mag_hash.getOrDefault(c,0)+1);
+        }
+        for(char c : ransomNote.toCharArray())
+        {
+            if(! mag_hash.containsKey(c) || mag_hash.get(c)<=0)
+            {
+                return false;
+            }
+            mag_hash.put(c,mag_hash.get(c)-1);
+        }
+        return true;
     }
 }
